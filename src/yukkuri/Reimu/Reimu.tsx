961c5f857a68b5f80d91f0968310ed9c0edfb320
kazuyaseki <track613armory@gmail.com>
2022-11-30 00:48:30 +0900

    added fuyofuyo animation

--- src/yukkuri/Reimu/Reimu.tsx
+++ src/yukkuri/Reimu/Reimu.tsx
@@ -1,4 +1,8 @@
 import {Img, staticFile} from 'remotion';
+import {
+	fuyofuyoAnimationCss,
+	FuyoFuyoAnimationStyle,
+} from '../fuyofuyoAnimation';
 import {eyeImagePaths} from './ReimuImagePaths/eyeImagePaths';
 import {faceImagePaths, FACE_TYPE} from './ReimuImagePaths/faceImagePaths';
 
@@ -12,62 +16,66 @@ const DEFAULT_REIMU_SIZE_PX = 320;
 
 export const Reimu: React.FC<Props> = ({face, eyeOpen, sizePx}) => {
 	return (
-		<div style={containerStyle}>
-			<Img
-				style={{width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`}}
-				src={staticFile('reimu/body/00.png')}
-			/>
-			{eyeOpen ? (
+		<>
+			<FuyoFuyoAnimationStyle />
+			<div style={containerStyle}>
 				<Img
-					style={{
-						...faceStyle,
-						width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`,
-					}}
-					src={staticFile(eyeImagePaths.open)}
+					style={{width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`}}
+					src={staticFile('reimu/body/00.png')}
 				/>
-			) : (
-				<Img
-					style={{
-						...faceStyle,
-						width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`,
-					}}
-					src={staticFile(eyeImagePaths.close)}
-				/>
-			)}
+				{eyeOpen ? (
+					<Img
+						style={{
+							...faceStyle,
+							width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`,
+						}}
+						src={staticFile(eyeImagePaths.open)}
+					/>
+				) : (
+					<Img
+						style={{
+							...faceStyle,
+							width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`,
+						}}
+						src={staticFile(eyeImagePaths.close)}
+					/>
+				)}
 
-			{eyeOpen ? (
-				<Img
-					style={{
-						...faceStyle,
-						width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`,
-					}}
-					src={staticFile('reimu/mouth/00.png')}
-				/>
-			) : (
-				<Img
-					style={{
-						...faceStyle,
-						width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`,
-					}}
-					src={staticFile('reimu/mouth/05.png')}
-				/>
-			)}
+				{eyeOpen ? (
+					<Img
+						style={{
+							...faceStyle,
+							width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`,
+						}}
+						src={staticFile('reimu/mouth/00.png')}
+					/>
+				) : (
+					<Img
+						style={{
+							...faceStyle,
+							width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`,
+						}}
+						src={staticFile('reimu/mouth/05.png')}
+					/>
+				)}
 
-			{face && (
-				<Img
-					style={{
-						...faceStyle,
-						width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`,
-					}}
-					src={staticFile(faceImagePaths[face])}
-				/>
-			)}
-		</div>
+				{face && (
+					<Img
+						style={{
+							...faceStyle,
+							width: `${sizePx ? sizePx : DEFAULT_REIMU_SIZE_PX}px`,
+						}}
+						src={staticFile(faceImagePaths[face])}
+					/>
+				)}
+			</div>
+		</>
 	);
 };
 
 const containerStyle: React.CSSProperties = {
 	position: 'relative',
+	...fuyofuyoAnimationCss,
 };
 
 const faceStyle: React.CSSProperties = {
---
+++ src/yukkuri/fuyofuyoAnimation.tsx
@@ -0,0 +1,23 @@
+const fuyofuyoFrequencyPx = 4;
+const fuyofuyoAnimationName = 'fuyofuyoAnimation';
+
+export const fuyofuyoAnimationCss: React.CSSProperties = {
+	animation: `${fuyofuyoAnimationName} 2s infinite ease-in-out`,
+	transform: `translateY(${fuyofuyoFrequencyPx}%)`,
+};
+
+export const FuyoFuyoAnimationStyle = () => (
+	<style>
+		{`@keyframes ${fuyofuyoAnimationName} {
+        0% {
+          transform: translateY(${fuyofuyoFrequencyPx}%);
+        }
+        50% {
+          transform: translateY(-${fuyofuyoFrequencyPx}%);
+        }
+        100% {
+          transform: translateY(${fuyofuyoFrequencyPx}%);
+        }
+      }`}
+	</style>
+);
